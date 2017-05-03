c3ce316379f01669e721a69a0c01c4b5ad4951fd
James Secor <james.secor@gmail.com>
2017-05-03 13:49:21 -0400

    edits to profile page

--- profile.php
+++ profile.php
@@ -7,8 +7,29 @@ include "acHeader.php";
 			<h1>User Profile Page</h1>
 			<p>
 			The user profile page displays all information about a user. Users can upload a profile picture that is displayed next to any
-			message or thread post.
+			message or thread post. Elements of the profile page include
 			</p>
+			<ul class="list-group">
+				<li class="list-group-item list-group-item-info">
+					<strong><span class="codeExample">First and Last Name</span></strong> Users are encouraged to use their actual first and last name.
+				</li>
+				<li class="list-group-item list-group-item-info">
+					<strong><span class="codeExample">Username</span></strong> The username is the name that will appear on all user posts, whether they be 
+					images, messages, posts to threads. The username is also how the system identifies a user. It is through the username that the system 
+					can load personalized pages.
+				</li>
+				<li class="list-group-item list-group-item-info">
+					<strong><span class="codeExample">Email</span></strong> This field is hidden from other users. ArtConnect keeps your email so that important
+					messages about the system or a user's account can be sent by email.
+				</li>
+				<li class="list-group-item list-group-item-info">
+					<strong><span class="codeExample">Profile Picture</span></strong> This image is an additional way that users are identified when they 
+					send messages, images, or post to a thread.
+				</li>
+				<li class="list-group-item list-group-item-info">
+					<strong><span class="codeExample">Preferences</span></strong>
+				</li>
+			</ul>
 			<p>
 			<!-- Users can see all threads by clicking <span class="codeExample">show all threads</span>.-->
 			</p>
